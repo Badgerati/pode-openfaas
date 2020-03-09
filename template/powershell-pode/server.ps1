0132c870ef474f29b7dec0fe3fe65159c0d199c2
Matthew Kelly <badgerati42@gmail.com>
2020-03-09 22:40:23 +0000

    minor tweak

--- template/powershell-pode/server.ps1
+++ template/powershell-pode/server.ps1
@@ -1,4 +1,4 @@
-Start-PodeServer -Threads 3 -Type Pode -ScriptBlock {
+Start-PodeServer -Threads 3 -ScriptBlock {
     Add-PodeEndpoint -Address 127.0.0.1 -Port 8082 -Protocol Http
     Use-PodeScript -Path './function/handler.ps1'
 }
